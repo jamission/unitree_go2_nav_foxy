c1b9ad1dc9e71718fd377de49857cdaf3e1371e2
sayantani <sayantanibhattacharya2025@u.northwestern.edu>
2025-02-21 00:54:02 -0600

    NavToPose msg type compressed.

--- unitree_go2_nav/src/navToPose.cpp
+++ unitree_go2_nav/src/navToPose.cpp
@@ -11,6 +11,7 @@
 #include "tf2/LinearMath/Matrix3x3.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 #include "nav2_msgs/action/navigate_to_pose.hpp"
+#include "unitree_go2_nav_interfaces/msg/nav_to_pose.hpp"
 
 // #include "/home/sayantani/Documents/Winter/project/ws2/src/unitree_go2_nav/unitree_ros2/cyclonedds_ws/unitree/unitree_go/msg/sport_mode_state.hpp"
 
@@ -50,15 +51,21 @@ class NavToPose : public rclcpp::Node
         // nav_start_trigger = create_service<std_srvs::srv::Empty>("nav_start_trigger", std::bind(&NavToPose::nav_start_trigger_callback, this, std::placeholders::_1));
         
         // Subsribers.
-        cmd_vel_sub = create_subscription<geometry_msgs::msg::Twist>("cmd_vel_nav", 10, std::bind(&NavToPose::cmd_vel_callback, this, std::placeholders::_1));
+        cmd_vel_sub = create_subscription<geometry_msgs::msg::Twist>("/cmd_vel_smoothed", 10, std::bind(&NavToPose::cmd_vel_callback, this, std::placeholders::_1));
         // the state_suber is set to subscribe "high_level_ctrl" topic
         // state_suber = this->create_subscription<unitree_go::msg::SportModeState>("sportmodestate", 10, std::bind(&NavToPose::state_callback, this, _1));
 
-
         // Publishers.
+        rclcpp::QoS qos_profile(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_default));
+        qos_profile.keep_last(50);  // Increase history depth
+        qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE);
+        qos_profile.history(RMW_QOS_POLICY_HISTORY_KEEP_LAST);
+        qos_profile.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);  // Or TRANSIENT_LOCAL if needed
+
         // The req_puber is set to subscribe "/api/sport/request" topic with dt
         // req_puber = this->create_publisher<unitree_api::msg::Request>("/api/sport/request", 10);
-        nav_msg_pub = this->create_publisher<geometry_msgs::msg::Twist>("nav_twist", 10);
+        nav_comp_msg_pub = this->create_publisher<unitree_go2_nav_interfaces::msg::NavToPose>("nav_twist", qos_profile);
+
         
         //Timer
         // std::chrono::milliseconds(int(dt * 1000))
@@ -74,21 +81,8 @@ class NavToPose : public rclcpp::Node
         // rclcpp::Service<std_srvs::srv::Empty>::SharedPtr nav_start_trigger;
         rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub;
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr nav_msg_pub;
+        rclcpp::Publisher<unitree_go2_nav_interfaces::msg::NavToPose>::SharedPtr nav_comp_msg_pub;
         geometry_msgs::msg::Twist cmd_vel;
-        // rclcpp::Subscription<unitree_go::msg::SportModeState>::SharedPtr state_suber;
-        // rclcpp::Publisher<unitree_api::msg::Request>::SharedPtr req_puber;
-        // unitree_api::msg::Request req; // Unitree Go2 ROS2 request message
-        // SportClient sport_req;
-        // double px0 = 0;  // initial x position
-        // double py0 = 0;  // initial y position
-        // double yaw0 = 0; // initial yaw angle
-
-        // void nav_start_trigger_callback(const std::shared_ptr<std_srvs::srv::Empty::Request> request, std::shared_ptr<std_srvs::srv::Empty::Response> response)
-        // {            
-        //     mRobotState = State::MANUAL_NAV_MODE;
-        //     RCLCPP_INFO(get_logger(), "Manual navigation mode started.");
-        //     response->success = true;
-        // }
 
         void cmd_vel_callback(const geometry_msgs::msg::Twist msg)
         {
@@ -101,34 +95,19 @@ class NavToPose : public rclcpp::Node
             // RCLCPP_INFO(get_logger(), "Cmd vel angular z/ YAW is %f", msg.angular.z);
         }
 
-        // void state_callback(unitree_go::msg::SportModeState::SharedPtr data)
-        // {
-        //     // Get current position of robot when t<0
-        //     // This position is used as the initial coordinate system
-        //     // Basically odom.
-
-        //     if (t < 0)
-        //     {
-        //         // Get initial position
-        //         px0 = data->position[0];
-        //         py0 = data->position[1];
-        //         yaw0 = data->imu_state.rpy[2];
-        //         std::cout << "[Odom]";
-        //         std::cout << px0 << ", " << py0 << ", " << yaw0 << std::endl;
-        //     }
-        // }
 
         void timer_callback()
         { 
             // RCLCPP_INFO(get_logger(), "Inside timer, robotsate is %f.", enum_cast<int>(mRobotState)); 
             if (mRobotState == State::IDLE) //MANUAL_NAV_MODE
             {
-                // std::vector<PathPoint> path;
-                // float vx = cmd_vel.linear.x;
-                // float vy = cmd_vel.linear.y;
-                // float vyaw = cmd_vel.angular.z;
-
                 nav_msg_pub->publish(cmd_vel);
+                
+                unitree_go2_nav_interfaces::msg::NavToPose msg;
+                msg.x = cmd_vel.linear.x;
+                msg.y = cmd_vel.linear.y;
+                msg.yaw = cmd_vel.angular.z;
+                nav_comp_msg_pub->publish(msg);
 
 
 
@@ -166,14 +145,4 @@ int main(int argc, char ** argv)
 
 
 // TO_DO:
-// I need a node that subscribes to :
-// geometry_msgs/msg/Twist  topic: /cmd_vel_nav
-// a trigger to start the navigation -> action client/service call probably.
-
-
-
-// use that to command the high level control and move the robot.
-
-// Nav might take care of that.
-// subscribe odom/base link lookup, to get the current pose
-// maybe write a control PID loop to move the robot to the desired pose.
+// a trigger to start the navigation -> service call.

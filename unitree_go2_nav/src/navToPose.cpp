e1b5055cce9010faf05fa344f11fc6472b907235
sayantani <sayantanibhattacharya2025@u.northwestern.edu>
2025-02-28 21:13:30 -0600

    Added checks and removed unused stuff.

--- unitree_go2_nav/src/navToPose.cpp
+++ unitree_go2_nav/src/navToPose.cpp
@@ -13,17 +13,8 @@
 #include "nav2_msgs/action/navigate_to_pose.hpp"
 #include "unitree_go2_nav_interfaces/msg/nav_to_pose.hpp"
 
-// #include "/home/sayantani/Documents/Winter/project/ws2/src/unitree_go2_nav/unitree_ros2/cyclonedds_ws/unitree/unitree_go/msg/sport_mode_state.hpp"
-
-// #include "unitree_api/msg/request.hpp"
-
-// /home/sayantani/Documents/Winter/project/ws2/src/unitree_go2_nav/unitree_ros2/example/src/include/common
-// #include "/home/sayantani/Documents/Winter/project/ws2/src/unitree_go2_nav/unitree_ros2/example/src/include/common/ros2_sport_client.h"
-
 using namespace std::chrono_literals;
 // using std::placeholders::_1;
-
-
 
 // State machine
 enum class State
@@ -44,16 +35,8 @@ class NavToPose : public rclcpp::Node
 
      NavToPose():Node("nav_to_pose")
      {       
-        //Action client: navigation 
-        // act_nav_to_pose_ = rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(this, "navigate_to_pose");
-        
-        // //Service call to trigger manual navigation: later need to do that msg type thing, see official documentation.
-        // nav_start_trigger = create_service<std_srvs::srv::Empty>("nav_start_trigger", std::bind(&NavToPose::nav_start_trigger_callback, this, std::placeholders::_1));
-        
         // Subsribers.
         cmd_vel_sub = create_subscription<geometry_msgs::msg::Twist>("/cmd_vel_smoothed", 10, std::bind(&NavToPose::cmd_vel_callback, this, std::placeholders::_1));
-        // the state_suber is set to subscribe "high_level_ctrl" topic
-        // state_suber = this->create_subscription<unitree_go::msg::SportModeState>("sportmodestate", 10, std::bind(&NavToPose::state_callback, this, _1));
 
         // Publishers.
         rclcpp::QoS qos_profile(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_default));
@@ -62,14 +45,10 @@ class NavToPose : public rclcpp::Node
         qos_profile.history(RMW_QOS_POLICY_HISTORY_KEEP_LAST);
         qos_profile.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);  // Or TRANSIENT_LOCAL if needed
 
-        // The req_puber is set to subscribe "/api/sport/request" topic with dt
-        // req_puber = this->create_publisher<unitree_api::msg::Request>("/api/sport/request", 10);
         nav_comp_msg_pub = this->create_publisher<unitree_go2_nav_interfaces::msg::NavToPose>("nav_twist", qos_profile);
 
         
         //Timer
-        // std::chrono::milliseconds(int(dt * 1000))
-        // t = -1; // Runing time count
         mTimer = this->create_wall_timer(100ms, std::bind(&NavToPose::timer_callback, this));      
 
 
@@ -78,17 +57,16 @@ class NavToPose : public rclcpp::Node
     private:
         rclcpp::TimerBase::SharedPtr mTimer;
         State mRobotState = State::IDLE;
-        // rclcpp::Service<std_srvs::srv::Empty>::SharedPtr nav_start_trigger;
         rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub;
-        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr nav_msg_pub;
         rclcpp::Publisher<unitree_go2_nav_interfaces::msg::NavToPose>::SharedPtr nav_comp_msg_pub;
         geometry_msgs::msg::Twist cmd_vel;
+        bool cmd_vel_received = false; 
 
         void cmd_vel_callback(const geometry_msgs::msg::Twist msg)
         {
             // geometry_msgs::msg::Twist cmd_vel;
             cmd_vel = msg;
-
+            cmd_vel_received = true;
 
             // RCLCPP_INFO(get_logger(), "Cmd vel x is %f", msg.linear.x);
             // RCLCPP_INFO(get_logger(), "Cmd vel y is %f", msg.linear.y);
@@ -99,17 +77,19 @@ class NavToPose : public rclcpp::Node
         void timer_callback()
         { 
             // RCLCPP_INFO(get_logger(), "Inside timer, robotsate is %f.", enum_cast<int>(mRobotState)); 
+            if (!cmd_vel_received)
+            {
+                RCLCPP_WARN(get_logger(), "[NavToPose] cmd_vel not received yet, skipping publishing.");
+                return;
+            }
+             
             if (mRobotState == State::IDLE) //MANUAL_NAV_MODE
             {
-                nav_msg_pub->publish(cmd_vel);
-                
                 unitree_go2_nav_interfaces::msg::NavToPose msg;
                 msg.x = cmd_vel.linear.x;
                 msg.y = cmd_vel.linear.y;
                 msg.yaw = cmd_vel.angular.z;
                 nav_comp_msg_pub->publish(msg);
-
-
 
                 // // Give a forward path.
                 // sport_req.Move(req, vx, vy, vyaw);
@@ -121,10 +101,8 @@ class NavToPose : public rclcpp::Node
             {
                 RCLCPP_ERROR(get_logger(), "Unhandled state.");
                 throw std::logic_error("Unhandled state.");
-            }
-            
+            }            
         }
-
 };
 
 int main(int argc, char ** argv)
@@ -135,14 +113,3 @@ int main(int argc, char ** argv)
   return 0;
 }
 
-
-
-
-
-
-
-
-
-
-// TO_DO:
-// a trigger to start the navigation -> service call.

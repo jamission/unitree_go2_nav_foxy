1d50bdf99b26573b8768efa381669db021144ba4
jamission <yo.mission.0511@gmail.com>
2025-07-01 11:14:05 -0400

    Update navToPose.cpp

--- unitree_go2_nav/src/navToPose.cpp
+++ unitree_go2_nav/src/navToPose.cpp
@@ -9,12 +9,13 @@
 #include "geometry_msgs/msg/quaternion.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2/LinearMath/Matrix3x3.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h" // Corrected for Foxy
 #include "nav2_msgs/action/navigate_to_pose.hpp"
 #include "unitree_go2_nav_interfaces/msg/nav_to_pose.hpp"
+#include "geometry_msgs/msg/twist.hpp" // Added for Twist message definition
 
 using namespace std::chrono_literals;
-// using std::placeholders::_1;
+// using std::placeholders::_1; // This line is commented out, which is fine as std::placeholders::_1 is used explicitly.
 
 // State machine
 enum class State
@@ -35,8 +36,14 @@ class NavToPose : public rclcpp::Node
 
      NavToPose():Node("nav_to_pose")
      {       
-        // Subsribers.
-        cmd_vel_sub = create_subscription<geometry_msgs::msg::Twist>("/cmd_vel_smoothed", 10, std::bind(&NavToPose::cmd_vel_callback, this, std::placeholders::_1));
+        // Subscribers.
+        // The template argument for create_subscription matches the message type.
+        // The callback signature will determine if it takes a value or a shared_ptr.
+        cmd_vel_sub = create_subscription<geometry_msgs::msg::Twist>(
+            "/cmd_vel_smoothed", 
+            10, 
+            std::bind(&NavToPose::cmd_vel_callback, this, std::placeholders::_1)
+        );
 
         // Publishers.
         rclcpp::QoS qos_profile(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_default));
@@ -48,29 +55,29 @@ class NavToPose : public rclcpp::Node
         nav_comp_msg_pub = this->create_publisher<unitree_go2_nav_interfaces::msg::NavToPose>("nav_twist", qos_profile);
 
         
-        //Timer
+        // Timer
         mTimer = this->create_wall_timer(100ms, std::bind(&NavToPose::timer_callback, this));      
-
-
      }       
 
     private:
         rclcpp::TimerBase::SharedPtr mTimer;
         State mRobotState = State::IDLE;
+        // The subscription member variable type is correct (SharedPtr to the message)
         rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub;
         rclcpp::Publisher<unitree_go2_nav_interfaces::msg::NavToPose>::SharedPtr nav_comp_msg_pub;
-        geometry_msgs::msg::Twist cmd_vel;
+        geometry_msgs::msg::Twist cmd_vel; // This member stores the actual message data (not a pointer)
         bool cmd_vel_received = false; 
 
-        void cmd_vel_callback(const geometry_msgs::msg::Twist msg)
+        // Corrected callback signature: takes a shared_ptr to a const message
+        void cmd_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
         {
-            // geometry_msgs::msg::Twist cmd_vel;
-            cmd_vel = msg;
+            // Dereference the shared_ptr to assign the message data to the member variable
+            cmd_vel = *msg; 
             cmd_vel_received = true;
 
-            // RCLCPP_INFO(get_logger(), "Cmd vel x is %f", msg.linear.x);
-            // RCLCPP_INFO(get_logger(), "Cmd vel y is %f", msg.linear.y);
-            // RCLCPP_INFO(get_logger(), "Cmd vel angular z/ YAW is %f", msg.angular.z);
+            RCLCPP_INFO(get_logger(), "Cmd vel x is %f", msg->linear.x);       // Use -> to access members of the shared_ptr
+            RCLCPP_INFO(get_logger(), "Cmd vel y is %f", msg->linear.y);       // Use -> to access members of the shared_ptr
+            RCLCPP_INFO(get_logger(), "Cmd vel angular z/ YAW is %f", msg->angular.z); // Use -> to access members of the shared_ptr
         }
 
 
@@ -86,7 +93,8 @@ class NavToPose : public rclcpp::Node
             if (mRobotState == State::IDLE) //MANUAL_NAV_MODE
             {
                 unitree_go2_nav_interfaces::msg::NavToPose msg;
-                msg.x = cmd_vel.linear.x;
+                // Here, cmd_vel is the geometry_msgs::msg::Twist object, so use .
+                msg.x = cmd_vel.linear.x; 
                 msg.y = cmd_vel.linear.y;
                 msg.yaw = cmd_vel.angular.z;
                 nav_comp_msg_pub->publish(msg);
@@ -112,4 +120,3 @@ int main(int argc, char ** argv)
   rclcpp::shutdown();
   return 0;
 }
-
